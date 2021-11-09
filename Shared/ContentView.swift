2f56bf9dede8c51dab86ffca525e17823113528f
bbulliard <bbulliard@gmail.com>
2021-11-09 09:31:32 -0600

    20211109_0931_works but shows mac simulator only

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,16 +10,37 @@ import Combine
 
 struct ContentView: View {
     @ObservedObject var taskStore = TaskStore()
+    @State var newToDo : String = ""
+    
+    var searchBar: some View {
+        HStack {
+            TextField("Enter in a new task", text: self.$newToDo)
+                .background(.gray)
+                .foregroundColor(.white)
+            Button(action: self.addNewToDo, label: {Text("Add New")
+                    .background(Color.gray)
+                    .foregroundColor(.white)
+            })
+        }
+    }
+    func addNewToDo()
+    {
+        taskStore.tasks.append(Task(id:
+                                        String(taskStore.tasks.count + 1),
+                                    todoitem: newToDo))
+        self.newToDo = ""    }
+    
     var body: some View {
         NavigationView {
             VStack {
+                searchBar.padding()
                 List(self.taskStore.tasks) { task in
                     Text(task.todoitem)
-                } .navigationBarTitle("Tasks")
-                    
+                } //.navigationBarTitle("Tasks")
+                
             }
         }
-       
+        
     }
 }
 
