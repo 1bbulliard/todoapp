ebc2597ca9a311bd63325ce4f5adfc6ae59c1373
bbulliard <bbulliard@gmail.com>
2021-11-08 19:52:11 -0600

    Initial Commit

---
+++ Shared/todoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoappApp.swift
+//  Shared
+//
+//  Created by Bob Bulliard on 11/8/21.
+//
+
+import SwiftUI
+
+@main
+struct todoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
